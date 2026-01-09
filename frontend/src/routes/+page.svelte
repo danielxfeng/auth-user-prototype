dd8a180981bed34ee490b0a35f3b2f61ea4b9999
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2026-01-10 00:05:05 +0200

    feat/frontend: homepage

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import { userStore } from '$lib/stores';
+	import { fade, fly } from 'svelte/transition';
+	import { onMount } from 'svelte';
+
+	let showTitle = false;
+
+	onMount(() => {
+		showTitle = true;
+	});
+</script>
+
+<div class="flex flex-1 flex-col items-center justify-center gap-10">
+	<div class="flex max-w-4xl flex-col items-center justify-center gap-12 p-4">
+		{#if showTitle}
+			<h1
+				class="text-center text-2xl font-semibold text-primary lg:text-3xl"
+				in:fly={{ y: 20, duration: 400, delay: 400 }}
+			>
+				Welcome to the Authentication Demo
+			</h1>
+
+			<div class="flex items-center justify-center" in:fade={{ duration: 500, delay: 600 }}>
+				{#if $userStore.user}
+					<a href="/profile/" role="button" class="btn"> Go to Profile </a>
+				{:else}
+					<div class="flex gap-4">
+						<a href="/login/" role="button" class="btn"> Login </a>
+						<a href="/register/" role="button" class="btn"> Register </a>
+					</div>
+				{/if}
+			</div>
+		{/if}
+	</div>
+</div>
