fef76846c765584912109d2de2dae72628bb77cd
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2026-01-10 21:22:08 +0200

    feat/frontend: delete account

---
+++ frontend/src/routes/user/settings/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { userStore } from '$lib/stores';
+	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
+	import DeleteAccount from './DeleteAccount.svelte';
+
+	onMount(() => {
+		if (!$userStore.user) {
+			goto('/user/login', { replaceState: true });
+		}
+	});
+</script>
+
+{#if $userStore.user}
+	<div class="flex flex-col gap-8 px-6">
+		<ChangePasswordForm />
+		<Logout />
+		<DeleteAccount />
+	</div>
+{/if}
---
+++ frontend/src/routes/user/settings/DeleteAccount.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import { userStore } from '$lib/stores';
+	import { goto } from '$app/navigation';
+	import { deleteAccount } from '$lib/service/authApiService';
+	import { toast } from 'svelte-sonner';
+	import { buttonVariants } from '$lib/components/ui/button/button.svelte';
+	import * as AlertDialog from '$lib/components/ui/alert-dialog/index.js';
+	import { Spinner } from '$lib/components/ui/spinner';
+
+	let deleting = false;
+
+	const deleteAccountHandler = async () => {
+		deleting = true;
+		try {
+			await deleteAccount();
+			userStore.logout();
+			toast.success('Account deleted successfully, navigating to home page...');
+			
+      setTimeout(() => {
+        goto('/');
+      }, 2000);
+		} catch {
+			toast.error('Failed to delete account. Please try again.');
+		} finally {
+			deleting = false;
+		}
+	};
+</script>
+
+<AlertDialog.Root>
+	<AlertDialog.Trigger class={buttonVariants({ variant: 'destructive' })}>
+		{#if deleting}
+			<Spinner /> Deleting...
+		{:else}
+			Delete Account
+		{/if}
+	</AlertDialog.Trigger>
+	<AlertDialog.Content>
+		<AlertDialog.Header>
+			<AlertDialog.Title>Are you absolutely sure?</AlertDialog.Title>
+			<AlertDialog.Description>
+				This action cannot be undone. This will permanently delete your account and remove your data
+				from our servers.
+			</AlertDialog.Description>
+		</AlertDialog.Header>
+		<AlertDialog.Footer>
+			<AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+			<AlertDialog.Action onclick={deleteAccountHandler} disabled={deleting}>Continue</AlertDialog.Action>
+		</AlertDialog.Footer>
+	</AlertDialog.Content>
+</AlertDialog.Root>
