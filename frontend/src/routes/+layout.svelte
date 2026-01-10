8f2dd2f1d670fb1d7a37d2333eede58b7009a92e
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2026-01-10 09:59:26 +0200

    feat/frontend: enhance layout and animations for better user experience

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import Toast from '$lib/components/Toast.svelte';
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-<div class="flex min-h-screen flex-col items-center justify-center">
+<div class="relative flex min-h-screen flex-col items-center justify-center">
 	{@render children()}
+	<Toast />
 </div>
