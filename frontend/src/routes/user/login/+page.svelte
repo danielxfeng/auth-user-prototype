b905eb8d52aabd9f9814725166ea8c1914d04c2f
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2026-01-10 16:17:43 +0200

    feat/frontend: add user redirection on mount for login and register pages

--- frontend/src/routes/user/login/+page.svelte
+++ frontend/src/routes/user/login/+page.svelte
@@ -1,11 +1,22 @@
 <script lang="ts">
+	import { on } from 'events';
 	import LoginForm from './LoginForm.svelte';
+	import { onMount } from 'svelte';
+	import { userStore } from '$lib/stores';
+	import { goto } from '$app/navigation';
 
 	let status: 'login' | '2fa' = 'login';
 
 	const goto2fa: () => void = () => {
 		status = '2fa';
 	};
+
+	onMount(() => {
+		if ($userStore.user) {
+			goto('/user/profile', { replaceState: true });
+		}
+	});
+	
 </script>
 
 <div class="px-6">
