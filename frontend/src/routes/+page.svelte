e1f4b0a27a61b199721cfc5883224b9a2ebec589
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2026-01-10 10:01:36 +0200

    fx/auth: prettier

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -5,12 +5,11 @@
 
 <div class="flex flex-1 flex-col items-center justify-center gap-10">
 	<div class="flex max-w-4xl flex-col items-center justify-center gap-12 p-4">
-		
-    <h1 class="animate-enter text-center text-2xl font-semibold lg:text-3xl xl:text-4xl">
+		<h1 class="animate-enter text-center text-2xl font-semibold lg:text-3xl xl:text-4xl">
 			Welcome to the Authentication Demo
 		</h1>
-		
-    {#if browser}
+
+		{#if browser}
 			<div class="flex animate-enter-soft items-center justify-center">
 				{#if $userStore.user}
 					<a href="/profile/" role="button" class="btn"> Go to Profile </a>
