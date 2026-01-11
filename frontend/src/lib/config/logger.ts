44f16e0a1c5a24b65d25587d45910888455da746
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2026-01-11 13:11:07 +0200

    chore/frontend: format logger methods for consistency and readability

--- frontend/src/lib/config/logger.ts
+++ frontend/src/lib/config/logger.ts
@@ -3,25 +3,25 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 interface Logger {
-  debug(...args: any[]): void;
-  info(...args: any[]): void;
-  warn(...args: any[]): void;
-  error(...args: any[]): void;
+	debug(...args: any[]): void;
+	info(...args: any[]): void;
+	warn(...args: any[]): void;
+	error(...args: any[]): void;
 }
 
 class ConsoleLogger implements Logger {
-  debug(...args: any[]): void {
-    console.debug(...args);
-  }
-  info(...args: any[]): void {
-    console.info(...args);
-  }
-  warn(...args: any[]): void {
-    console.warn(...args);
-  }
-  error(...args: any[]): void {
-    console.error(...args);
-  }
+	debug(...args: any[]): void {
+		console.debug(...args);
+	}
+	info(...args: any[]): void {
+		console.info(...args);
+	}
+	warn(...args: any[]): void {
+		console.warn(...args);
+	}
+	error(...args: any[]): void {
+		console.error(...args);
+	}
 }
 
 export const logger: Logger = new ConsoleLogger();
