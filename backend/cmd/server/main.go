8433761c8be1d1965f5245ccfc60944d84ca5d5b
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2026-02-04 19:37:13 +0200

    refactor/backend: move router setup to a dedicated file for better organization

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
-	"strings"
 	"syscall"
 	"time"
 
@@ -23,51 +22,8 @@ import (
 
 	"log/slog"
 
-	sloggin "github.com/samber/slog-gin"
-
-	"github.com/gin-contrib/cors"
-
 	"github.com/paularynty/transcendence/auth-service-go/internal/dependency"
-	"github.com/paularynty/transcendence/auth-service-go/internal/middleware"
 )
-
-func SetupRouter(dep *dependency.Dependency) *gin.Engine {
-	r := gin.New()
-
-	logConfig := sloggin.Config{
-		DefaultLevel:     slog.LevelInfo,
-		ClientErrorLevel: slog.LevelWarn,
-		ServerErrorLevel: slog.LevelError,
-	}
-
-	// A rough CORS
-	r.Use(cors.New(cors.Config{
-		AllowOriginFunc: func(origin string) bool {
-			if origin == "http://localhost:5173" ||
-				origin == "http://localhost:4173" {
-				return true
-			}
-			if strings.HasSuffix(origin, ".vercel.app") {
-				return true
-			}
-			return false
-		},
-		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
-	}))
-
-	rateLimiter := middleware.NewRateLimiter(time.Duration(dep.Cfg.RateLimiterDurationInSec)*time.Second, dep.Cfg.RateLimiterRequestLimit, time.Duration(dep.Cfg.RateLimiterCleanupIntervalInSec)*time.Second)
-	r.Use(rateLimiter.RateLimit())
-
-	r.Use(middleware.PanicHandler())
-	r.Use(sloggin.NewWithConfig(dep.Logger, logConfig))
-	r.Use(middleware.ErrorHandler())
-
-	return r
-}
 
 // @title Auth Service API
 // @version 1.0
@@ -97,7 +53,7 @@ func main() {
 	}
 
 	// router
-	r := SetupRouter(dep)
+	r := routers.SetupRouter(dep)
 	routers.UsersRouter(r.Group("/api/users"), userService)
 
 	// Health check
